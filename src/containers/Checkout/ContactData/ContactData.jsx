3637f2f74800b292b5ff8ea406d7e1420be01c21
batar-btr <btrn86@gmail.com>
2018-04-11 15:35:59 +0300

    adding form validation

--- src/containers/Checkout/ContactData/ContactData.jsx
+++ src/containers/Checkout/ContactData/ContactData.jsx
@@ -15,7 +15,11 @@ class ContactData extends Component {
                     placeholder: 'Your Name ...',
 
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false
             },
             street: {
                 elementType: 'input',
@@ -24,7 +28,11 @@ class ContactData extends Component {
                     placeholder: 'Your Street ...',
 
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false
             },
             zipCode: {
                 elementType: 'input',
@@ -33,7 +41,13 @@ class ContactData extends Component {
                     placeholder: 'Your ZIP code ...',
 
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true,
+                    minLength: 5,
+                    maxLength: 5
+                },
+                valid: false
             },
             country: {
                 elementType: 'input',
@@ -42,7 +56,11 @@ class ContactData extends Component {
                     placeholder: 'Your Country ...',
 
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false
             },
             email: {
                 elementType: 'input',
@@ -51,7 +69,11 @@ class ContactData extends Component {
                     placeholder: 'Your E-mail ...',
 
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false
             },
             deliveryMethod: {
                 elementType: 'select',
@@ -87,6 +109,21 @@ class ContactData extends Component {
             .catch(err => this.setState({ loading: false }));
     }
 
+    checkValidity = (value, rules) => {
+        let isValid = true;
+
+        if(rules.required) {
+            isValid = value.trim() !== '' && isValid;
+        }
+        if(rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid
+        }
+        if(rules.maxLength) {
+            isValid = value.length <= rules.maxLength && isValid
+        }
+        return isValid;
+    }
+
     inputChangedHandler = (event, inputIdentifier) => {
         const updatedOrderForm = {
             ...this.state.orderForm
@@ -95,7 +132,9 @@ class ContactData extends Component {
             ...updatedOrderForm[inputIdentifier]
         }
         updateFormElement.value = event.target.value
+        updateFormElement.valid = this.checkValidity(updateFormElement.value, updateFormElement.validation)
         updatedOrderForm[inputIdentifier] = updateFormElement
+        console.log(updateFormElement)
         this.setState({ orderForm: updatedOrderForm })
     }
 
