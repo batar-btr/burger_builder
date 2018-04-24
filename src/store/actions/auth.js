bda1c9500496f52dcb749979bb18d22e224773c4
batar-btr <btrn86@gmail.com>
2018-04-24 17:45:40 +0300

    getting token from backend

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,21 +1,36 @@
 import * as actionTypes from './actionTypes'
+import axios from 'axios'
 
 export const authStart = () => ({
     type: actionTypes.AUTH_START
-}) 
+})
 
 export const authSuccess = authData => ({
     type: actionTypes.AUTH_SUCCESS,
     authData: authData
-}) 
+})
 
 export const authFail = error => ({
     type: actionTypes.AUTH_FAIL,
     error: error
-}) 
+})
 
 export const auth = (email, password) => {
     return dispatch => {
         dispatch(authStart())
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        }
+        axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyD2RIfNXZthi2MrbR5GrxUtpnvTbsmQ5zk', authData)
+            .then(res => {
+                console.log(res.data)
+                dispatch(authSuccess(res.data))
+            })
+            .catch(err => {
+                console.log(err)
+                dispatch(authFail(err))
+            })
     }
 }
